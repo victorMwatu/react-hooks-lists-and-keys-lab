20655b84c24902b12b5986a6d2d8c51f6b690015
victorMwatu <victormwatu@gmail.com>
2025-07-16 14:10:29 +0300

    complete react-hooks-lists-and-keys-lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,9 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+          {links.map((l, i) => <a key={i} href={`#${l}`}> {l} </a>)}
+  </nav>;
 }
 
 export default NavBar;
